949a5a98f8bbb3071a331abededd90ba6ac1bcfb
Tony Stark <afemiadom@icloud.com>
2022-12-31 11:43:15 -0600

    NationCred contract WIP

--- contracts/ActiveCitizens.sol
+++ contracts/ActiveCitizens.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+// To-Do: Change access control mechanism
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ActiveCitizens is Ownable {
-    address[] citizens;
+    address[] private citizens;
 
     /**
-     * @notice push an address to the array
+     * @notice push an address to nation3 active citizen array
      * @dev if the address already exists, it should not be added again
      */
-    function _addCitizen(address addr) private onlyOwner {
+    function _addCitizen(address addr) public onlyOwner {
+        // Impliment a better way to add addresses. This isn't gas efficient
         citizens.push(addr);
     }
-
 
     /**
      * @notice check if citizen already exist on the active citizen list.
@@ -23,7 +24,13 @@ contract ActiveCitizens is Ownable {
     /**
      * @notice remove an address from the array
      */
-    function _removeCitizen(address addr) private onlyOwner {}
+    function _removeCitizen(address addr) private {}
 
-    function viewActiveCitizen() public view returns (address[] memory) {}
+    /**
+     * @notice view address of active citizens
+     */
+    function viewActiveCitizen() public view returns (address[] memory) {
+        // not the best solution for array that will grow indefinately
+        return citizens;
+    }
 }
