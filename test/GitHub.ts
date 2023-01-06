98de4521da68d5fa94af2b82b2415c6a17a49821
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-01-06 18:04:43 +0800

    test: update username (#3)

--- test/GitHub.ts
+++ test/GitHub.ts
@@ -6,5 +6,26 @@ describe("GitHub", function () {
     const GitHub = await ethers.getContractFactory("GitHub");
     const gitHub = await GitHub.deploy();
     await gitHub.deployed();
+
+    const username = await gitHub.usernames(
+      "0x4e3072f7b5C075EA5FdEb423DA95312C4B99dc22"
+    );
+    console.log("username:", username);
+    expect(username).to.equal("aahna-ashina");
+  });
+
+  it("updateUsername", async function () {
+    const GitHub = await ethers.getContractFactory("GitHub");
+    const gitHub = await GitHub.deploy();
+    await gitHub.deployed();
+
+    const [owner] = await ethers.getSigners();
+    console.log("owner.address:", owner.address);
+
+    await gitHub.updateUsername("test");
+
+    const usernameAfterUpdate = await gitHub.usernames(owner.address);
+    console.log("usernameAfterUpdate:", usernameAfterUpdate);
+    expect(usernameAfterUpdate).to.equal("test");
   });
 });
