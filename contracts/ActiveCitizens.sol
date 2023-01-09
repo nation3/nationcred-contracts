c8a334864f4b077cd1bf1d98ff50f3687a5e45d2
Tony Stark <afemiadom@icloud.com>
2023-01-09 14:56:42 -0600

    Merkle Tree WIP

--- contracts/ActiveCitizens.sol
+++ contracts/ActiveCitizens.sol
@@ -2,35 +2,62 @@
 pragma solidity ^0.8.17;
 
 // To-Do: Change access control mechanism
-import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract ActiveCitizens is Ownable {
-    address[] private citizens;
+contract ActiveCitizens {
+    bytes32 public root;
 
-    /**
-     * @notice push an address to nation3 active citizen array
-     * @dev if the address already exists, it should not be added again
-     */
-    function _addCitizen(address addr) public onlyOwner {
-        // Impliment a better way to add addresses. This isn't efficient
-        citizens.push(addr);
+    // address[] citizens;
+
+    constructor(bytes32 _root) {
+        root = _root;
     }
 
-    /**
-     * @notice check if citizen already exist on the active citizen list.
-     */
-    function exists() internal view returns (bool) {}
+    // /**
+    //  * @notice push an address to nation3 active citizen array
+    //  * @dev if the address already exists, it should not be added again
+    //  */
+    // function addCitizen(address addr) public onlyOwner {
+    //     // Impliment a better way to add addresses. This isn't efficient
+    //     citizens.push(addr);
+    // }
 
-    /**
-     * @notice remove an address from the array
-     */
-    function _removeCitizen(address addr) private {}
+    // /**
+    //  * @notice check if citizen already exist on the active citizen list.
+    //  */
+    // function exists() private view returns (bool) {}
 
-    /**
-     * @notice view address of active citizens
-     */
-    function viewActiveCitizen() public view returns (address[] memory) {
-        // not the best solution for array that will grow indefinately
-        return citizens;
+    // /**
+    //  * @notice remove an address from the array
+    //  */
+    // function removeCitizen(address addr) public onlyOwner {}
+
+    // /**
+    //  * @notice view address of active citizens
+    //  */
+    // function viewActiveCitizen() public view returns (address[] memory) {
+    //     // not the best solution for array that will grow indefinately
+    //     return citizens;
+    // }
+
+    // function _leaf(address account) internal pure returns (bytes32) {
+    //     return keccak256(abi.encodePacked(account));
+    // }
+
+    // function _verify(bytes32 leaf, bytes32[] memory proof)
+    //     internal
+    //     view
+    //     returns (bool)
+    // {
+    //     return MerkleProof.verify(proof, root, leaf);
+    // }
+
+    function isValid(bytes32[] memory proof, bytes32 leaf)
+        public
+        view
+        returns (bool)
+    {
+        return MerkleProof.verify(proof, root, leaf);
     }
 }
---
+++ utils/merkle.ts
@@ -0,0 +1,21 @@
+const { MerkleTree } = require("merkletreejs")
+const keccak256 = require("keccak256")
+
+export function createActiveCitizen() {
+
+    const padBuffer = (addr: any) => {
+        return Buffer.from(addr.substr(2).padStart(32 * 2, 0), 'hex')
+    }
+    console.log("This is a Merkle tree for Nation3 active citizen")
+
+    let activeCitizen = [
+        // Get list from active-citizen csv.
+    ]
+
+    const leaves = activeCitizen.map(addr => padBuffer(addr))
+    const merkletree = new MerkleTree(leaves, keccak256, { sortPairs: true })
+    console.log("merkletree", merkletree.toString())
+    const rootHash = merkletree.getHexRoot()
+    console.log("root hash", rootHash)
+    return rootHash
+} 
