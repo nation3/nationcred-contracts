fb2f210e2486aaf4ab0fcb5c0ce99a8dd187764e
Tony Stark <afemiadom@icloud.com>
2023-01-10 16:33:04 -0600

    Added keccak256 dependency

--- test/NationCred.ts
+++ test/NationCred.ts
@@ -1,26 +1,26 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+// import { expect } from "chai";
+// import { ethers } from "hardhat";
 
-describe("NationCred contract", function () {
-  async function deployActiveCitizens() {
-    const NationCred = await ethers.getContractFactory("NationCred");
-    const nationCred = await NationCred.deploy();
-    await nationCred.deployed();
-  }
+// describe("NationCred contract", function () {
+//   async function deployNationCred() {
+//     const NationCred = await ethers.getContractFactory("NationCred");
+//     const nationCred = await NationCred.deploy();
+//     await nationCred.deployed();
+//   }
 
-  it("Should add new citizen to list of active citizens", async function () {
+//   it("Should add new citizen to list of active citizens", async function () {
 
-  });
+//   });
 
-  it("Should check if citizen already exist on active citizen list", async function () {
+//   it("Should check if citizen already exist on active citizen list", async function () {
 
-  });
+//   });
 
-  it("Should remove citizen's address from list of active citizen", async function () {
+//   it("Should remove citizen's address from list of active citizen", async function () {
 
-  });
+//   });
 
-  it("Should be able to view addresses of all active citizens", async function () {
+//   it("Should be able to view addresses of all active citizens", async function () {
 
-  });
-});
+//   });
+// });
