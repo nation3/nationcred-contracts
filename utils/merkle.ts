4a64298667b6c6046f7aa2ad2ef068316cf302e5
Tony Stark <afemiadom@icloud.com>
2023-01-10 16:40:07 -0600

    Changed linter to prettier

--- utils/merkle.ts
+++ utils/merkle.ts
@@ -1,20 +1,19 @@
-const { MerkleTree } = require("merkletreejs")
-const keccak256 = require("keccak256")
+const { MerkleTree } = require("merkletreejs");
+const keccak256 = require("keccak256");
 
 export function createActiveCitizen() {
-
     const padBuffer = (addr: any) => {
-        return Buffer.from(addr.substr(2).padStart(32 * 2, 0), 'hex')
-    }
-    console.log("This is a Merkle tree for Nation3 active citizen")
+        return Buffer.from(addr.substr(2).padStart(32 * 2, 0), "hex");
+    };
+    console.log("This is a Merkle tree for Nation3 active citizen");
 
     // Get list from active-citizen csv.
-    let activeCitizen = []
+    let activeCitizen = [];
 
-    const leaves = activeCitizen.map(addr => padBuffer(addr))
-    const merkletree = new MerkleTree(leaves, keccak256, { sortPairs: true })
-    console.log("merkletree", merkletree.toString())
-    const rootHash = merkletree.getHexRoot()
-    console.log("root hash", rootHash)
-    return rootHash
-} 
+    const leaves = activeCitizen.map((addr) => padBuffer(addr));
+    const merkletree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+    console.log("merkletree", merkletree.toString());
+    const rootHash = merkletree.getHexRoot();
+    console.log("root hash", rootHash);
+    return rootHash;
+}
