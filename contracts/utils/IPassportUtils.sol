f2bf9a2351afa186d12712082147adbc695f0fea
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-09-29 20:11:53 +0800

    feat: checking passport status (#72)
    close #72

---
+++ contracts/utils/IPassportUtils.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+interface IPassportUtils {
+    // TO DO
+    // isPassportOwner
+
+    /**
+     * Returns `true` if a citizen's vote-escrowed `$NATION` balance has 
+     * dropped below the passport expiry threshold; `false` otherwise.
+     *
+     * @param citizen The address of the passport owner
+     */
+    function isPassportExpired(address citizen) external view returns (bool);
+
+    // TO DO
+    // getPassportExpirationDate
+}
