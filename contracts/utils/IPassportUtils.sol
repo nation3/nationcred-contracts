4b7902ea13b827bea23667fc0d17bef63a9e40f3
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-10-05 19:06:52 +0700

    feat: smart contract for checking passport status (#72)
    close #72

---
+++ contracts/utils/IPassportUtils.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+interface IPassportUtils {
+  /**
+   * Returns `true` if a citizen's passport has become revocable.
+   *
+   * @param passportID The NFT passport ID
+   */
+  function isExpired(uint16 passportID) external view returns (bool);
+
+  // /**
+  //  * Returns the Unix epoch time when the passport will become revocable.
+  //  * 
+  //  * @param passportID The NFT passport ID
+  //  */
+  // function getExpirationTimestamp(uint16 passportID) external view returns (uint256);
+}
---
+++ contracts/utils/PassportUtils.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+import "./IPassportUtils.sol";
+
+contract PassportUtils is IPassportUtils {
+  function isExpired(uint16 passportID) public view returns (bool) {
+    return true;
+  }
+}
