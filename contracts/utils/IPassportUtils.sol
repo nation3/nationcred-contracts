c970258970bf9d7463571134f5408de9d65d2959
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-10-05 20:10:25 +0700

    npx prettier

--- contracts/utils/IPassportUtils.sol
+++ contracts/utils/IPassportUtils.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.17;
 
 interface IPassportUtils {
-  /**
-   * Returns `true` if a citizen's passport has become revocable.
-   *
-   * @param passportID The NFT passport ID
-   */
-  function isExpired(uint16 passportID) external view returns (bool);
+    /**
+     * Returns `true` if a citizen's passport has become revocable.
+     *
+     * @param passportID The NFT passport ID
+     */
+    function isExpired(uint16 passportID) external view returns (bool);
 
-  // /**
-  //  * Returns the Unix epoch time when the passport will become revocable.
-  //  * 
-  //  * @param passportID The NFT passport ID
-  //  */
-  // function getExpirationTimestamp(uint16 passportID) external view returns (uint256);
+    // /**
+    //  * Returns the Unix epoch time when the passport will become revocable.
+    //  *
+    //  * @param passportID The NFT passport ID
+    //  */
+    // function getExpirationTimestamp(uint16 passportID) external view returns (uint256);
 }
--- contracts/utils/PassportUtils.sol
+++ contracts/utils/PassportUtils.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "./IPassportUtils.sol";
 
 contract PassportUtils is IPassportUtils {
-  function isExpired(uint16 passportID) public view returns (bool) {
-    return true;
-  }
+    function isExpired(uint16 passportID) public view returns (bool) {
+        return true;
+    }
 }
