f3d6324e0437669b764c9d4ccc7c124e82138ddf
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-10-22 22:40:40 +0700

    refactor: npx eslint (#70)
    #70

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,6 @@
-require('dotenv').config()
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
--- test/GitHubUsernames.ts
+++ test/GitHubUsernames.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
-describe("GitHubUsernames", function() {
+describe("GitHubUsernames", function () {
   async function deploymentFixture() {
     console.log("deploymentFixture");
 
@@ -29,40 +29,37 @@ describe("GitHubUsernames", function() {
     return { owner, otherAccount, githubUsernames };
   }
 
-  it("usernames empty", async function() {
-    const { owner, githubUsernames } = await loadFixture(
-      deploymentFixture
-    );
+  it("usernames empty", async function () {
+    const { owner, githubUsernames } = await loadFixture(deploymentFixture);
 
     const username = await githubUsernames.usernames(owner.address);
     console.log("username:", username);
     expect(username).to.equal("");
   });
 
-  it("updateUsername - citizen with valid passport", async function() {
-    const { owner, githubUsernames } = await loadFixture(
-      deploymentFixture
-    );
+  it("updateUsername - citizen with valid passport", async function () {
+    const { owner, githubUsernames } = await loadFixture(deploymentFixture);
 
     const tx = await githubUsernames.updateUsername("New Username");
-    console.log('tx:', tx);
+    console.log("tx:", tx);
 
     const usernameAfterUpdate = await githubUsernames.usernames(owner.address);
     console.log("usernameAfterUpdate:", usernameAfterUpdate);
     expect(usernameAfterUpdate).to.equal("New Username");
   });
 
-  it("updateUsername - citizen with expired passport", async function() {
+  it("updateUsername - citizen with expired passport", async function () {
     const { otherAccount, githubUsernames } = await loadFixture(
       deploymentFixture
     );
-    
-    await expect(githubUsernames.connect(otherAccount).updateUsername("Other Username")).to.be.revertedWithCustomError(
-      githubUsernames,
-      "PassportExpired"
-    );
 
-    const usernameAfterUpdate = await githubUsernames.usernames(otherAccount.address);
+    await expect(
+      githubUsernames.connect(otherAccount).updateUsername("Other Username")
+    ).to.be.revertedWithCustomError(githubUsernames, "PassportExpired");
+
+    const usernameAfterUpdate = await githubUsernames.usernames(
+      otherAccount.address
+    );
     console.log("usernameAfterUpdate:", usernameAfterUpdate);
     expect(usernameAfterUpdate).to.equal("");
   });
