19f37e470c3b5ed8c52c6d43027a07019176f8d4
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-10-22 16:21:53 +0700

    feat: require valid passport - github (#70)
    #70

--- contracts/GitHub.sol
+++ contracts/GitHub.sol
@@ -1,32 +1,26 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 
-import { IVotingEscrow } from "./governance/IVotingEscrow.sol";
+import {IVotingEscrow} from "./governance/IVotingEscrow.sol";
+import {IPassportUtils} from "./utils/IPassportUtils.sol";
 
 contract GitHub {
     mapping(address => string) public usernames;
     IVotingEscrow public veToken;
+    IPassportUtils public passportUtils;
 
     error PassportExpired();
 
-    constructor(address veToken_) {
+    constructor(address veToken_, address passportUtils_) {
         veToken = IVotingEscrow(veToken_);
+        passportUtils = IPassportUtils(passportUtils_);
     }
 
     function updateUsername(string calldata username) public {
-        if (isPassportExpired(msg.sender)) {
+        if (passportUtils.isExpired(msg.sender)) {
             revert PassportExpired();
         } else {
             usernames[msg.sender] = username;
-        }
-    }
-
-    function isPassportExpired(address passportOwner) public view returns (bool) {
-        uint256 veTokenBalance = veToken.balanceOf(passportOwner);
-        if (veTokenBalance < (1.5 * 1e18)) {
-            return false;
-        } else {
-            return true;
         }
     }
 }
