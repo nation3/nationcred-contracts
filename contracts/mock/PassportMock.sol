784c67226366c5de0236edc74c7f74e49485999d
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-02-25 21:05:26 +0800

    chore: openzeppelin v5 (#108)
    close #108

--- contracts/mock/PassportMock.sol
+++ contracts/mock/PassportMock.sol
@@ -3,18 +3,15 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract PassportMock is ERC721, Ownable {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIdCounter;
+    uint256 private _tokenIdCounter;
 
     constructor() ERC721("Nation3 Genesis Passport", "PASS3") {}
 
     function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
+        _safeMint(to, _tokenIdCounter);
     }
 }
