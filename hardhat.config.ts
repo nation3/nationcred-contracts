eb2603767574ce349cd09868c2bc117413d9c3d5
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-04-12 23:51:43 +0800

    🫥 chore: upgrade to solidity 0.8.25

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,7 +17,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.21",
+    version: "0.8.25",
     settings: {
       optimizer: {
         enabled: true,
