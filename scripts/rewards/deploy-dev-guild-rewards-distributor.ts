6641e55bf87a825101d3d600bcebd902dc27a440
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-04-15 18:27:35 +0800

    chore: set mainnet addresses

--- scripts/rewards/deploy-dev-guild-rewards-distributor.ts
+++ scripts/rewards/deploy-dev-guild-rewards-distributor.ts
@@ -6,8 +6,9 @@ async function main() {
 
   // Constructor Args
   // const passportUtilsAddress = "0x88Ea3A3618A988783E39C2CadFdd77Dc07895b59"; // Sepolia
+  // const rewardTokenAddress = "0x23Ca3002706b71a440860E3cf8ff64679A00C9d7"; // Sepolia
   const passportUtilsAddress = "0x23Ca3002706b71a440860E3cf8ff64679A00C9d7"; // Mainnet
-  const rewardTokenAddress = "0x23Ca3002706b71a440860E3cf8ff64679A00C9d7"; // Sepolia
+  const rewardTokenAddress = "0x333A4823466879eeF910A04D473505da62142069"; // Mainnet
   const CLIFF_VESTING_DATE = 1735689600; // 2025-01-01
 
   const args = [passportUtilsAddress, rewardTokenAddress, CLIFF_VESTING_DATE];
